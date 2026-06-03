7c82b916c4a60602bfe1bbe341ecb68013ab865e
Yohannesmulu <yohannesmulu@gmail.com>
2026-06-03 17:27:29 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ session=cnx.session()
 st.title(f":cup_with_straw: Customize Your Smoothie!:cup_with_straw: ")
 st.write("Orders that need to be filled.")
 
-session = get_active_session()
+# session = get_active_session()
 my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED") == 0).collect()
 
 if my_dataframe:
